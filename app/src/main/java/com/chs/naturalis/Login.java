ce975ef4f1352689dd2f1652571735f9c8659c0d
Marius Roncea <marius.roncea@geotogether.com>
2022-10-23 00:05:43 +0300

    Animations for logo added

---
+++ app/src/main/java/com/chs/naturalis/Login.java
@@ -0,0 +1,17 @@
+package com.chs.naturalis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chs.naturalis.R;
+
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
