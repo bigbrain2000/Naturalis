cecfa6ba3d74155e3ffc112fcd59aa54e6470f5d
Mester Rebeca <rebeca.mester9@gmail.com>
2022-11-06 15:08:35 +0200

    Home Page for Client and Admin

--- app/src/main/java/com/chs/naturalis/HomePageClient.java
+++ app/src/main/java/com/chs/naturalis/HomePageClient.java
@@ -1,14 +1,16 @@
 package com.chs.naturalis;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class HomePageClient extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_client);
+
     }
 }
